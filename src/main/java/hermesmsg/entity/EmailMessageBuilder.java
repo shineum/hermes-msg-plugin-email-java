f05f7316ea6da1057c25aea1671edfba424f974b
Sungwon Um <shineum@gmail.com>
2025-03-01 22:37:44 -0800

    add getter and some methods

--- src/main/java/hermesmsg/entity/EmailMessageBuilder.java
+++ src/main/java/hermesmsg/entity/EmailMessageBuilder.java
@@ -1,5 +1,6 @@
 package hermesmsg.entity;
 
+import hermesmsg.util.DataConverter;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,76 +22,74 @@ public class EmailMessageBuilder {
     boolean isHtml = false;
     List<ByteArrayAttachment> attachments = new ArrayList<>();
 
-    EmailMessageBuilder setFrom(String from) {
+    public EmailMessageBuilder setFrom(String from) {
         this.from = from;
         return this;
     }
 
-    EmailMessageBuilder setTO(String to) {
+    public EmailMessageBuilder setTo(String to) {
         this.to = to;
         return this;
     }
 
-    EmailMessageBuilder setCC(String cc) {
+    public EmailMessageBuilder setCc(String cc) {
         this.cc = cc;
         return this;
     }
 
-    EmailMessageBuilder setBCC(String bcc) {
+    public EmailMessageBuilder setBcc(String bcc) {
         this.bcc = bcc;
         return this;
     }
 
-    EmailMessageBuilder setSubject(String subject) {
+    public EmailMessageBuilder setSubject(String subject) {
         this.subject = subject;
         return this;
     }
 
-    EmailMessageBuilder setBody(String body, boolean isHtml) {
+    public EmailMessageBuilder setBody(String body, boolean isHtml) {
         this.body = body;
         this.isHtml = isHtml;
         return this;
     }
 
-    EmailMessageBuilder addAttachmentFile(File file, String contentType) {
+    public EmailMessageBuilder addAttachmentFile(File file, String contentType) {
         return addAttachment(new ByteArrayAttachment(file, contentType));
     }
 
-    EmailMessageBuilder addAttachmentByteArray(String filename, byte[] filedata, String contentType) {
+    public EmailMessageBuilder addAttachmentByteArray(String filename, byte[] filedata, String contentType) {
         return addAttachment(new ByteArrayAttachment(filename, filedata, contentType));
     }
 
-    EmailMessageBuilder addAttachment(ByteArrayAttachment byteArrayAttachment) {
+    public EmailMessageBuilder addAttachment(ByteArrayAttachment byteArrayAttachment) {
         attachments.add(byteArrayAttachment);
         return this;
     }
 
-    EmailMessage build() {
+    public EmailMessage build() {
         return new EmailMessage(this);
     }
 
     private String addRecipient(String prev, String newRecipient) {
-        return Optional.ofNullable(prev)
-                .map(recipient -> recipient + "," + newRecipient)
-                .orElse(newRecipient);
+        return Optional.ofNullable(prev).map(recipient -> recipient + "," + newRecipient).orElse(newRecipient);
     }
 
-    EmailMessageBuilder addTO(String to) {
+    public EmailMessageBuilder addTO(String to) {
         this.to = addRecipient(this.to, to);
         return this;
     }
 
-    EmailMessageBuilder addCC(String cc) {
+    public EmailMessageBuilder addCC(String cc) {
         this.cc = addRecipient(this.cc, cc);
         return this;
     }
 
-    EmailMessageBuilder addBCC(String bcc) {
+    public EmailMessageBuilder addBCC(String bcc) {
         this.bcc = addRecipient(this.bcc, bcc);
         return this;
     }
 
-    EmailMessageBuilder setWithMap(Map<String, String> dataMap) {
+    public EmailMessageBuilder setWithMap(Map<String, String> dataMap) {
         this.from = dataMap.get(EmailMessage.EM_KEY_FROM);
         this.to = dataMap.get(EmailMessage.EM_KEY_TO);
         this.cc = dataMap.get(EmailMessage.EM_KEY_CC);
@@ -101,7 +100,7 @@ public class EmailMessageBuilder {
         return this;
     }
 
-    EmailMessageBuilder setWithProperties(Properties props) {
+    public EmailMessageBuilder setWithProperties(Properties props) {
         this.from = props.getProperty(EmailMessage.EM_KEY_FROM);
         this.to = props.getProperty(EmailMessage.EM_KEY_TO);
         this.cc = props.getProperty(EmailMessage.EM_KEY_CC);
@@ -112,7 +111,7 @@ public class EmailMessageBuilder {
         return this;
     }
 
-    EmailMessageBuilder setWithPropertiesStr(String propStr) {
+    public EmailMessageBuilder setWithPropertiesStr(String propStr) {
         try {
             Properties props = new Properties();
             props.load(new StringReader(propStr));
@@ -123,7 +122,7 @@ public class EmailMessageBuilder {
         return this;
     }
 
-    EmailMessageBuilder setWithJSONObject(JSONObject jo) {
+    public EmailMessageBuilder setWithJSONObject(JSONObject jo) {
         this.from = jo.has(EmailMessage.EM_KEY_FROM) ? jo.getString(EmailMessage.EM_KEY_FROM) : null;
         this.to = jo.has(EmailMessage.EM_KEY_TO) ? jo.getString(EmailMessage.EM_KEY_TO) : null;
         this.cc = jo.has(EmailMessage.EM_KEY_CC) ? jo.getString(EmailMessage.EM_KEY_CC) : null;
@@ -134,8 +133,58 @@ public class EmailMessageBuilder {
         return this;
     }
 
-    EmailMessageBuilder setWithJSONStr(String jsonStr) {
+    public EmailMessageBuilder setWithJSONStr(String jsonStr) {
         setWithJSONObject(new JSONObject(jsonStr));
         return this;
+    }
+
+    public static EmailMessage fromMessageStr(String msgStr) {
+        try {
+            return EmailMessageJOSerializer.fromJSONObject(new JSONObject(new String(DataConverter.getDecompressedData(DataConverter.base64Decode(msgStr)), "UTF-8")));
+        } catch (Exception e) {
+            logger.error(e.toString());
+        }
+        return null;
+    }
+
+    public static String convertToMessageStr(EmailMessage emailMessage) {
+        try {
+            return DataConverter.base64Encode(DataConverter.getCompressedData(EmailMessageJOSerializer.serialize(emailMessage).toString().getBytes("UTF-8")));
+        } catch (Exception e) {
+            logger.error(e.toString());
+        }
+        return null;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public String getCc() {
+        return cc;
+    }
+
+    public String getBcc() {
+        return bcc;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public boolean isHtml() {
+        return isHtml;
+    }
+
+    public List<ByteArrayAttachment> getAttachments() {
+        return attachments;
     }
 }
